8e1cee46162e96e69fe6a1c6173fc496901716b5
Fredrik Lindgren <lindgren.fredrik@gmail.com>
2013-05-31 19:29:41 +0200

    Initial commit
    A lightly pruned version of Roy's edits to Elkhound, modified by
    Valerio Bigiani and originally by Scott McPeak

---
+++ src/elkhound/c/treeout.h
@@ -0,0 +1,15 @@
+// treeout.h            see license.txt for copyright and terms of use
+// interface for printing trees; very small right now
+
+// the idea is to print ascii marked in a way that Emacs' outline
+// mode can read and display it as a tree
+
+#ifndef TREEOUT_H
+#define TREEOUT_H
+
+#include <iostream>       // std::ostream
+
+// print heading to stdout, with some indication of tree level
+std::ostream &treeOut(int level);
+
+#endif // TREEOUT_H
